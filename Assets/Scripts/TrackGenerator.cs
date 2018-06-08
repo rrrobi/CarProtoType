3d32a203e7033393fe8f1eb3838b8258c675bcff
rrrobi <rrrrobi@msn.com>
2018-06-09 00:24:46 +0100

    made a start on track generator

---
+++ Assets/Scripts/TrackGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackGenerator : MonoBehaviour {
+
+
+    public GameObject trackPieceToSpawn;
+    Vector3 spawnPos;
+    Quaternion spawnRot;
+    int trackCount = 0;
+
+	// Use this for initialization
+	void Start () {
+        spawnPos = new Vector3(0.0f, 0.0f, 0.0f);
+        spawnRot = Quaternion.identity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetMouseButtonDown(0))
+        {
+            spawnPos = new Vector3(0.0f, 0.0f, trackCount * 50);
+            Instantiate(trackPieceToSpawn, spawnPos, spawnRot);
+            trackCount++;
+        }
+	}
+}
